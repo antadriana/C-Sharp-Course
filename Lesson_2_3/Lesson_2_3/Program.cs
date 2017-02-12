2383780ee379374af812e66116834c342704b89d
antadriana <adriana98@ukr.net>
2017-02-12 08:55:02 +0200

    Added Lessons 2-3

---
+++ Lesson_2_3/Lesson_2_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Lesson_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* float km = 15.9f;
+             string val = Console.ReadLine();
+             Console.WriteLine(val+" Line Readed");*/
+            Console.WriteLine("Please, enter number of km");
+            float km = float.Parse(Console.ReadLine());
+            float m = km * 1000;
+            Console.WriteLine("\a");
+            Console.WriteLine("{2} km= {1} m", km, m, 15, "hello");
+            Console.WriteLine($"{km} km={m} m");
+                Console.WriteLine(km+"km= "+m+"m");
+            Console.WriteLine("enter Pin:****\b\b\b\b");
+            int pin = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
---
+++ Lesson_2_3_1/Lesson_2_3_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Lesson_2_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string product1, product2, product3;
+            float price1, price2, price3;
+            string name1;
+            Console.WriteLine("Please, enter the products: ");
+            product1 = Console.ReadLine();
+            product2 = Console.ReadLine();
+            product3 = Console.ReadLine();
+
+            Console.WriteLine("Please, enter the prices: ");
+            price1 = float.Parse(Console.ReadLine());
+            price2 = float.Parse(Console.ReadLine());
+            price3 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please, enter the waiter: ");
+            name1 = Console.ReadLine();
+            Console.WriteLine("Bill:" + " " + name1 + "\n" +" "+product1 + " $" + price1);
+            Console.WriteLine( " "+product2 + " $" + price2);
+            Console.WriteLine( " " + product3 + " $" + price3);
+            Console.WriteLine("____________________________");
+            Console.WriteLine("sum:" + " $" + (price1 + price2 + price3));
+        }
+    }
+}
