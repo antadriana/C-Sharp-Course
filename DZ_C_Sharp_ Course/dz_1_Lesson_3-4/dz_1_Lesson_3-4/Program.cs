707e4447a382850c9e0ee4a726bf0c2531a2df76
antadriana <adriana98@ukr.net>
2017-02-12 09:03:25 +0200

    Added dz 2-3

---
+++ DZ_C_Sharp_ Course/dz_1_Lesson_3-4/dz_1_Lesson_3-4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz_1_Lesson_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter three marks: ");
+            int firstMark, secondMark, thirdMark;
+            firstMark = int.Parse(Console.ReadLine());
+            secondMark = int.Parse(Console.ReadLine());
+            thirdMark = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The first mark is: "+firstMark);
+            Console.WriteLine("The second mark is: "+secondMark);
+            Console.WriteLine("The thirs mark is: " +thirdMark);
+            Console.WriteLine("The average value is: "+ ((firstMark+secondMark+thirdMark)/3));
+        }
+    }
+}
